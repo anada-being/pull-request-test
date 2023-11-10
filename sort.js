32ea25108d08e7894d8b88515ca4d0ef3ef9fedb
anada <anada@beingcorp.co.jp>
2023-11-10 13:27:33 +0900

    配列すをソートする処理を追加しました

---
+++ sort.js
@@ -0,0 +1,11 @@
+var sortNumber = function(nunmber){
+number,sort(function(a,b)){
+	if(a==b){
+		return0;
+	}
+));
+};
+
+var number = [19, 3, 81, 1, 24, 21];
+sortNumber(nunmber);
+console.log(number);
